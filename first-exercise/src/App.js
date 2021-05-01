9f0837da97f480318536a17b06dbd95f068bd78d
LucasMLara <lucasml@live.com>
2021-05-01 20:47:03 -0300

    part 1 first exercise implement

--- first-exercise/src/App.js
+++ first-exercise/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
+const task = (value) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <li>{value}</li>
   );
 }
+const array = ['zuar','estudar','se desesperar', 'tentar', 'falhar']
 
+class App extends React.Component {
+  render() {
+  return (
+    <ul>
+        {array.map((palavra) => task(palavra))}
+    </ul>
+  );
+  }
+}
 export default App;
