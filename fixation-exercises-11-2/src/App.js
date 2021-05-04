7f3d514109443b4a6fda99488c7591d95a630d8f
LucasMLara <lucasml@live.com>
2021-05-04 11:39:56 -0300

    ImageFnc Implement

--- fixation-exercises-11-2/src/App.js
+++ fixation-exercises-11-2/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Image from './Image.js';
+import ImageFnc from './ImageFnc';
 
 function App() {
   return (
     <div className="App">
 <Image source="https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg" 
-alternativeText="VAITOMARNOCU" corno="Vinicius Bigodin"/>
+alternativeText="CuteKitty"/>
+<ImageFnc source="https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg" 
+alternativeText="CuteKitty"/>
     </div>
   );
 }
