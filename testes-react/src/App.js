d417575430653dac9b4653288e263bb3f3f3da6b
LucasMLara <lucasml@live.com>
2021-05-22 17:00:49 -0300

    adding todo array

--- testes-react/src/App.js
+++ testes-react/src/App.js
@@ -5,7 +5,7 @@ const task = (value) => {
     <li>{value}</li>
   );
 }
-const toDo = ['Tomar café da manhã', 'Limpar a sujeira da cachorra', 'Lavar o Banheiro', 'Varrer e passar pano na casa', 'Estudar de 10h as 22h de segunda a sexta', 'Fazer tudo igual no sábado, mas estudando só até 18h']
+const toDo = ['Tomar café da manhã', 'Limpar a sujeira da cachorra', 'Lavar o Banheiro', 'Varrer e passar pano na casa', 'Estudar de 10h as 22h de segunda a sexta', 'Fazer tudo igual no sábado, mas estudando só até 18h'];
 
 function App() {
   return (
