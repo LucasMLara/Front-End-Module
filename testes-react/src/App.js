4eef28136f1a74582f37eff52c4426aa7f6c00a3
LucasMLara <lucasml@live.com>
2021-05-22 22:26:48 -0300

    OrderList

--- testes-react/src/App.js
+++ testes-react/src/App.js
@@ -1,30 +1,34 @@
 import React from 'react';
-import './App.css'
-import UserProfile from './UserProfile';
+import './App.css';
+import Order from './Order';
 
 class App extends React.Component {
   render() {
-
-    // Declaração do objeto contendo informações do album02
-    const joao = {
+    const headphone = {
       id: 102,
-      name: "João",
-      email: "joao@gmail.com",
-      avatar: "https:cdn.pixabay.com/photo/2016/08/20/05/38/avatar-1606916_640.png"
+      user: "cena@gmail.com",
+      product: "Razer Headphone",
+      price: {
+        value: 99.99,
+        currency: "dollars"
+      }
     };
 
-    const amelia = {
+    const energyDrink = {
       id: 77,
-      name: "Amélia",
-      email: "amelia@gmail.com",
-      avatar: "https:cdn.pixabay.com/photo/2017/01/31/13/05/cameo-2023867_640.png"
+      user: "cena@gmail.com",
+      product: "Monster 500mL",
+      price: {
+        value: 9.99,
+        currency: "dollars"
+      }
     };
 
-    // Retorno do que será renderizado
     return (
-      <div>
-        <UserProfile user={joao} />
-        <UserProfile user={amelia} />
+      <div className="App">
+        <h1> Orders recently created </h1>
+        <Order order={ headphone }/>
+        <Order order={ energyDrink }/>
       </div>
     );
   }
